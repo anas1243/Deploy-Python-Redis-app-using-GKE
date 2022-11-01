77133d8ebc17f8ea53ba88ad80af7faab48e03be
Anas <anas1243@gmail.com>
2022-11-01 22:46:39 +0200

    feat: Add NAT in the private subnet of the cluster

--- terraform/nat.tf
+++ terraform/nat.tf
@@ -1,13 +1,27 @@
-resource "google_compute_router" "iti-router" {
-  name    = "${var.env}-iti-router"
+resource "google_compute_router" "iti-router-vm-subnet" {
+  name    = "${var.env}-iti-router-vm-subnet"
   region  = var.private-subnet1-region
   network = google_compute_network.iti-network.id
 }
 
-resource "google_compute_router_nat" "iti-nat" {
-  name                               = "${var.env}-iti-nat"
-  router                             = google_compute_router.iti-router.name
+resource "google_compute_router_nat" "iti-nat-vm-subnet" {
+  name                               = "${var.env}-iti-nat-vm-subnet"
+  router                             = google_compute_router.iti-router-vm-subnet.name
   region                             = var.private-subnet1-region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
+
+resource "google_compute_router" "iti-router-cluster-subnet" {
+  name    = "${var.env}-iti-router-cluster-subnet"
+  region  = var.private-subnet2-region
+  network = google_compute_network.iti-network.id
+}
+
+resource "google_compute_router_nat" "iti-nat-cluster-subnet" {
+  name                               = "${var.env}-iti-nat-cluster-subnet"
+  router                             = google_compute_router.iti-router-cluster-subnet.name
+  region                             = var.private-subnet2-region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
