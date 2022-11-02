471a8f0d04b977adec7fa6ca81ae3f63e871f4a4
Anas <anas1243@gmail.com>
2022-11-02 13:56:07 +0200

    fix: Fix jenkins configurations including service account and volumes

--- terraform/GKE.tf
+++ terraform/GKE.tf
@@ -15,6 +15,23 @@ resource "google_container_cluster" "iti-gke" {
     master_global_access_config {
       enabled = true
     }
+    # startup_script = <<-EOT
+    #   # Install docker
+    #   sudo apt-get update
+    #   sudo apt-get install ca-certificates curl gnupg lsb-release  
+    #   sudo mkdir -p /etc/apt/keyrings
+    #   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    #   echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    #   sudo apt-get update
+    #   sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    #   sudo apt-get update
+    #   sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    #   sudo usermod -aG docker $USER
+    #   newgrp docker
+
+    #   # make jenkins home directory
+    #   mkdir ~/jenkins
+    # EOT
 
   }
   master_authorized_networks_config {
@@ -30,10 +47,11 @@ resource "google_container_node_pool" "iti-node-pool" {
   name       = "${var.env}-iti-node-pool"
   location   = var.private-subnet2-region
   cluster    = google_container_cluster.iti-gke.name
+
   node_count = 1
   node_config {
     machine_type = "e2-medium"
-
+    
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.bastion-svc-account.email
     oauth_scopes = [
